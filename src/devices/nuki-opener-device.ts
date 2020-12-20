6840611376e0ebd826d779b2d5c7efd38a3cd86b
Domenico Pata <d.pata@binary-factory.de>
2020-12-20 03:14:29 +0100

    wip on opener device
    added rto and continuous Mode switches and implementes update on calbacks

--- src/devices/nuki-opener-device.ts
+++ src/devices/nuki-opener-device.ts
@@ -4,6 +4,7 @@ import { NukiBridgeApi } from '../api/nuki-bridge-api';
 import { NukiDeviceTypes } from '../api/nuki-device-types';
 import { NukiDeviceState } from '../api/nuki-device-state';
 import { NukiOpenerAction } from '../api/nuki-opener-action';
+import { NukiOpenerState } from '../api/nuki-opener-state';
 
 
 export class NukiOpenerDevice extends AbstractNukIDevice {
@@ -12,6 +13,10 @@ export class NukiOpenerDevice extends AbstractNukIDevice {
     private readonly _lockService: Service;
 
     private readonly _doorRingSignalService: Service;
+
+    private readonly _rtoSwitchService: Service;
+
+    private readonly _continuousModeSwitchService: Service;
 
     private readonly _characteristic: any;
 
@@ -38,8 +43,26 @@ export class NukiOpenerDevice extends AbstractNukIDevice {
         this._doorRingSignalService = this.getOrAddService(api.hap.Service.Lightbulb, 'Door-Ring Indicator');
 
         this._doorRingSignalService.getCharacteristic(api.hap.Characteristic.On)
-            .on('get', this.handleDoorRingGet.bind(this))   //todo richitig so ?
+            .on('get', this.handleDoorRingGet.bind(this))
             .on('set', this.handleDoorRingSet.bind(this));
+
+        //todo: check if config is user want this
+        if (true) {
+            this._rtoSwitchService = this.getOrAddService(api.hap.Service.Switch, 'Ring to Open', 'ringToOpen');
+            this._continuousModeSwitchService = this.getOrAddService(api.hap.Service.Switch, 'Continuous Mode', 'continuousMode');
+
+        }
+
+        // Subscribes for changes of the RTO mode
+        if (this._rtoSwitchService) {
+            this._rtoSwitchService.getCharacteristic(this._characteristic.On).on('set', this.handleRtoSwitchServiceSet.bind(this));
+        }
+
+        // Subscribes for changes of the continuous mode
+        if (this._continuousModeSwitchService) {
+            this._continuousModeSwitchService.getCharacteristic(this._characteristic.On).on('set',
+                this.handleContinuousModeSwitchServiceSet.bind(this));
+        }
 
         this._informationService
             .setCharacteristic(api.hap.Characteristic.Manufacturer, 'Nuki')
@@ -49,7 +72,85 @@ export class NukiOpenerDevice extends AbstractNukIDevice {
 
     update(lastKnownState: NukiDeviceState) {
         this._log.debug('lastKnownState Opener', lastKnownState);
+
+        // Checks if the state exists, which is not the case if the device is unavailable
+        if (!lastKnownState) {
+            return;
+        }
+
+        // Sets the lock state
+
+        if (lastKnownState.state === NukiOpenerState.ONLINE || lastKnownState.state === NukiOpenerState.RTO_ACTIVE) {
+            this._log(this.id + ' - Updating lock state: SECURED/SECURED');
+            this._lockService.updateCharacteristic(this._characteristic.LockCurrentState, this._characteristic.LockCurrentState.SECURED);
+            this._lockService.updateCharacteristic(this._characteristic.LockTargetState, this._characteristic.LockTargetState.SECURED);
+        }
+
+        if (lastKnownState.state === NukiOpenerState.OPEN) {
+            this._log(this.id + ' - Updating lock state: UNSECURED/UNSECURED');
+            this._lockService.updateCharacteristic(this._characteristic.LockCurrentState, this._characteristic.LockCurrentState.UNSECURED);
+            this._lockService.updateCharacteristic(this._characteristic.LockTargetState, this._characteristic.LockTargetState.UNSECURED);
+        }
+        if (lastKnownState.state == NukiOpenerState.OPENING) {
+            this._log(this.id + ' - Updating lock state: -/UNSECURED');
+            this._lockService.updateCharacteristic(this._characteristic.LockTargetState, this._characteristic.LockTargetState.UNSECURED);
+        }
+
+        // Sets the ring action state
+        if (this._doorRingSignalService && lastKnownState.ringactionState &&
+            (lastKnownState.state !== NukiOpenerState.RTO_ACTIVE || lastKnownState.mode !== 3)) {
+
+            this._log(this.id + ' - Updating doorbell: Ring');
+            this._doorRingSignalService.setCharacteristic(this._characteristic.On, true);
+        }
+
+        // Sets the status for the continuous mode
+        if (this._continuousModeSwitchService) {
+            this._log.debug(this.id + ' - Updating Continuous Mode: ' + lastKnownState.mode);
+            this._continuousModeSwitchService.updateCharacteristic(this._characteristic.On, lastKnownState.mode === 3);
+        }
+
+        // Sets the status for RTO
+        if (this._rtoSwitchService) {
+            if (lastKnownState.state === NukiOpenerState.ONLINE || lastKnownState.state === NukiOpenerState.RTO_ACTIVE) {
+                this._log.debug(this.id + ' - Updating RTO: ' + lastKnownState.state);
+                this._rtoSwitchService.updateCharacteristic(this._characteristic.On, lastKnownState.state === NukiOpenerState.RTO_ACTIVE);
+            }
+        }
+
+        // Sets the status of the battery
+        this._log.debug(this.id + ' - Updating critical battery: ' + lastKnownState.batteryCritical);
+        if (lastKnownState.batteryCritical) {
+            this._lockService.updateCharacteristic(this._characteristic.StatusLowBattery,
+                this._characteristic.StatusLowBattery.BATTERY_LEVEL_LOW);
+        } else {
+            this._lockService.updateCharacteristic(this._characteristic.StatusLowBattery,
+                this._characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL);
+        }
     }
+
+    handleRtoSwitchServiceSet(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+        this._log.debug(' Opener - Set RTO to ' + value);
+        this._nukiApi.lockAction(this.id, NukiDeviceTypes.Opener,
+            value ? NukiOpenerAction.ACTIVATE_RTO : NukiOpenerAction.DEACTIVATE_RTO).then(() => {
+            callback(null);
+        }).catch((err) => {
+            this._log.error(err);
+            callback(err);
+        });
+    }
+
+    handleContinuousModeSwitchServiceSet(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+        this._log.debug(' Opener - Set Continuous Mode  to ' + value);
+        this._nukiApi.lockAction(this.id, NukiDeviceTypes.Opener,
+            value ? NukiOpenerAction.ACTIVATE_CONTINUOUS_MODE : NukiOpenerAction.DEACTIVATE_CONTINUOUS_MODE).then(() => {
+            callback(null);
+        }).catch((err) => {
+            this._log.error(err);
+            callback(err);
+        });
+    }
+
 
     handleDoorRingGet(callback: CharacteristicSetCallback) {
         this._log.info('Current state of the switch was returned: ' + (this.bellRingLightOn ? 'ON' : 'OFF'));
