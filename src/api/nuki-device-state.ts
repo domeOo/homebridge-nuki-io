f50517299a18f610dd6fd57824245ee7b2a70012
Domenico Pata <d.pata@binary-factory.de>
2020-12-20 03:13:35 +0100

    added ringactionTimestamp and ..state to nukie-device-states.ts

--- src/api/nuki-device-state.ts
+++ src/api/nuki-device-state.ts
@@ -1,11 +1,14 @@
 import { NukiLockState } from './nuki-lock-state';
 import { NukiDoorSensorState } from './nuki-door-sensor-state';
+import { NukiOpenerState } from "./nuki-opener-state";
 
 export interface NukiDeviceState {
     mode: number;
-    state: NukiLockState;
+    state: NukiLockState | NukiOpenerState;
     stateName: string;
     batteryCritical: boolean;
     doorsensorState: NukiDoorSensorState;
     doorsensorStateName: string;
+    ringactionTimestamp: Date;
+    ringactionState: boolean;
 }
